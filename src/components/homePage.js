220ce33807a84c7c00c06aab7e77291daa6d06c8
Bilal Taherkheli <bt@configit.com>
2015-12-28 14:00:44 +0100

    module 4 done. how to create a react component and why we need react router later

---
+++ src/components/homePage.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var React = require('react');
+
+var Home = React.createClass({
+  render: function(){
+    return (
+      <div className="jumbotron">
+        <h1>Pluralsight Administration</h1>
+        <p>React, React Router. and Flux for ultra-responsive web apps.</p>
+        <p>Allah keeta ....</p>
+      </div>
+    );
+  }
+});
+
+module.exports = Home;
